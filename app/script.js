37744b8e43dfd25b42d3b4ab52f8d6c740da8d01
Vincent de La Fonchais <vdelafonchais@itnetwork.fr>
2023-12-22 14:15:47 +0100

    Accessible responsible nav

--- app/script.js
+++ app/script.js
@@ -1,4 +1,4 @@
-var matches = document.querySelectorAll('.dropdown');
+const matches = document.querySelectorAll('.dropdown');
 matches.forEach(element=>{
   element.addEventListener("focusin", (event) => {
     element.querySelector("button").setAttribute("aria-expanded", true);
@@ -13,3 +13,22 @@ matches.forEach(element=>{
     element.querySelector("button").blur();
   });
 })
+
+// Responsiveness. On label (burger menu) focus, check the according checkbox (if unchecked)
+const menuTogglerLabel = document.querySelector('#menu-toggler-label');
+const menuTogglerCheckbox = document.querySelector('#menu-toggler-checkbox');
+menuTogglerLabel.addEventListener("focus", (event) => {
+  if(menuTogglerCheckbox.checked === false){
+    menuTogglerCheckbox.checked = true;
+  }else{
+    menuTogglerCheckbox.checked = false;
+  }
+});
+// Same on click
+menuTogglerLabel.addEventListener("click", (event) => {
+  if(menuTogglerCheckbox.checked === false){
+    menuTogglerCheckbox.checked = true;
+  }else{
+    menuTogglerCheckbox.checked = false;
+  }
+});
